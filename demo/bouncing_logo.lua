6912688bf461c4d01f2e2b9aaf476a656c4ea480
Rodrigo Carreirão <rodrigo.carreirao@gmail.com>
2025-07-04 22:50:39 -0300

    Commiting a full resource loader inside lua script. It could be edited through editor.

--- demo/bouncing_logo.lua
+++ demo/bouncing_logo.lua
@@ -8,6 +8,7 @@ local LuaBouncingLogo = {
 	
 	-- Declare properties
 	linear_velocity = export(100),
+	count = 0,
 	initial_angle = export({
 		type = float,
 		default = 0,
@@ -19,7 +20,7 @@ local LuaBouncingLogo = {
 }
 -- Called when the node enters the scene tree for the first time.
 function LuaBouncingLogo:_ready()
-    print("Ready lua----------------------")
+	print("Ready lua----------------------")
 	--self.position = self:get_viewport():get_size() / 2
 	-- self.movement = Vector2(self.linear_velocity, 0):rotated(deg_to_rad(self.initial_angle))
 end
@@ -30,26 +31,38 @@ end
 
 function LuaBouncingLogo:_rods()
 	print("Rods")
-    local n = Node:new()
-    --self:add_child(n)
-    print(self)
-    print(n:is_inside_tree())  -- false
-    --n:queue_free()
-    --local sprite = Sprite2D:new()
+	local n = Node:new()
+	--self:add_child(n)
+	print(self)
+	print(n:is_inside_tree())  -- false
+	--n:queue_free()
+	--local sprite = Sprite2D:new()
 
-    --Load and assign a texture
-    -- sprite.texture = self:load("res://icon_green.png")
+	--Load and assign a texture
+	-- sprite.texture = self:load("res://icon_green.png")
 
-    --Set position (optional)
-    --sprite.position = Vector2(5, -5)
+	--Set position (optional)
+	--sprite.position = Vector2(5, -5)
 
-    --Add the sprite to the current scene
-    return 1
+	--Add the sprite to the current scene
+	return 1
 end
 
 -- Called every frame. 'delta' is the elapsed time since the previous frame.
 function LuaBouncingLogo:_process(delta)
-    -- print("_process lua")
+	-- print("_process lua")
+	--print(self.count)
+	if(self.count == 0) then
+		local sprite = Sprite2D:new()
+
+		sprite.texture = ResourceLoader:load("res://icon_green.png")
+		print("Exists")
+		print(ResourceLoader:exists("res://icon_green.png"))
+
+		sprite.position = Vector2(150, 5)
+		self:add_child(sprite)
+	end
+	self.count = self.count + 1
 	-- local viewport_size = self:get_viewport():get_size()
 	-- local viewport_rect = Rect2(Vector2(), viewport_size)
 	-- if not viewport_rect:encloses(self.global_transform * self:get_rect()) then
